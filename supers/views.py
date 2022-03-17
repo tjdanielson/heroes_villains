02a1c127e64b8dfb3bd8017394825af87b5e5d5e
tjdanielson <tessajdanielson@gmail.com>
2022-03-17 13:28:56 -0500

    Created a delete method in SuperDetail class to delete a super.

--- supers/views.py
+++ supers/views.py
@@ -41,5 +41,10 @@ class SuperDetail(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk):
+        super = self.get_object(pk)
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     
 
