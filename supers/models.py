72f9780ddfd4a6e5fef3068905df7f8665495ffb
tjdanielson <tessajdanielson@gmail.com>
2022-03-17 15:29:47 -0500

    Replaced primary and secondary ability fields with many to many power fields in Super model, updated super serializer with above change as well. Migrated changes to db.

--- supers/models.py
+++ supers/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from super_types.models import SuperType
 
 # Create your models here.
+class Power(models.Model):
+    name = models.CharField(max_length=100)
+
 class Super(models.Model):
     name = models.CharField(max_length=100)
     alter_ego = models.CharField(max_length=100)
-    primary_ability = models.CharField(max_length=100)
-    secondary_ability = models.CharField(max_length=100)
+    powers = models.ManyToManyField(Power)
+    # primary_ability = models.CharField(max_length=100)
+    # secondary_ability = models.CharField(max_length=100)
     catchphrase = models.CharField(max_length=240)
     super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
 
-class Power(models.Model):
-    name = models.CharField(max_length=100)
+
