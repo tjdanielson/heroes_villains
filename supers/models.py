1384dd518e3eb6b22db9fd61dd2508d8c5828a7e
tjdanielson <tessajdanielson@gmail.com>
2022-03-17 11:41:30 -0500

    Added serializer for supers model.

--- supers/models.py
+++ supers/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from super_types.models import SuperType
 
 # Create your models here.
---
+++ supers/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Super
+
+class SuperSerializer(serializers.Serializer):
+    class Meta:
+        model = Super
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        
+
+
