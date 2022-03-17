b5cb896b3dce5eb723fbc5fc18024f6cbe2fbb76
tjdanielson <tessajdanielson@gmail.com>
2022-03-17 11:33:22 -0500

    Created supers model, added supers to project settings installed apps, migrated supers model to database.

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from super_types.models import SuperType
+
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=100)
+    alter_ego = models.CharField(max_length=100)
+    primary_ability = models.CharField(max_length=100)
+    secondary_ability = models.CharField(max_length=100)
+    catchphrase = models.CharField(max_length=240)
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
