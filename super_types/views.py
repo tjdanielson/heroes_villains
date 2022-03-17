79859d77c5fd2d3d06d54570363bbc7d3e653201
tjdanielson <tessajdanielson@gmail.com>
2022-03-17 15:06:22 -0500

    Add put method in SuperTypeDetails class to be able to update super types.

--- super_types/views.py
+++ super_types/views.py
@@ -34,3 +34,11 @@ class SuperTypeDetails(APIView):
         serializer = SuperTypeSerializer(super_type)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def put(self, request, pk):
+        super_type = self.get_object(pk)
+        serializer = SuperTypeSerializer(super_type, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
