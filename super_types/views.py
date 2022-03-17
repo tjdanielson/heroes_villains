c8b777cb4e4c0a4ee8a1186a10b05f1d34ec9494
tjdanielson <tessajdanielson@gmail.com>
2022-03-17 15:02:51 -0500

    Added method for posting a new super type in the SuperTypeList class.

--- super_types/views.py
+++ super_types/views.py
@@ -13,6 +13,13 @@ class SuperTypeList(APIView):
         serializer = SuperTypeSerializer(super_types, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        serializer = SuperTypeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 class SuperTypeDetails(APIView):
 
