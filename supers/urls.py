2a04890c0251fe3fd31496d6352cda05976b7f50
tjdanielson <tessajdanielson@gmail.com>
2022-03-17 11:58:08 -0500

    Created view for getting super by id/pk, added url to url paths and project urls.

--- supers/urls.py
+++ supers/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('<int:pk>/', views.SuperDetail.as_view())
+]
--- supers/views.py
+++ supers/views.py
@@ -1,4 +1,26 @@
-from rest_framework.decorators import api_view
-
+from rest_framework.decorators import APIView
+from .serializers import SuperSerializer
+from .models import Super
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class SuperList(APIView):
+
+    def get(self, request):
+        pass
+
+class SuperDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Super.objects.get(pk=pk)
+        except Super.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
